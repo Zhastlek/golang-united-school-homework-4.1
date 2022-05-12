060618cbef2a9b8e9376c7fb4e7dc6d97cbe6d03
zhastlek <zhastlek95@gmail.com>
2022-05-12 21:22:38 +0600

    reverse string

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,9 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	str := []rune(input)
+	for i := len(str) - 1; i >= 0; i-- {
+		output += string(str[i])
+	}
 	return output
 }
